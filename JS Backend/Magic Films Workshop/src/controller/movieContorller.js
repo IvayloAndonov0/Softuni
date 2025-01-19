6be8b683e0937bc53aa81d5eb6c64325e212358c
Ivaylo Andonov <nikb250nik@gmail.com>
2025-01-19 13:53:00 +0200

    added movies controller and create page

---
+++ JS Backend/Magic Films Workshop/src/controller/movieContorller.js
@@ -0,0 +1,13 @@
+import { Router } from "express";
+import movies from "../movies.js"
+
+const movieController = Router();
+
+movieController.get(`/create`,(req,res)=>{
+    res.render(`create`)
+})
+
+
+
+
+export default movieController;
--- JS Backend/Magic Films Workshop/src/routes.js
+++ JS Backend/Magic Films Workshop/src/routes.js
@@ -1,8 +1,10 @@
 import homeConroller from "./controller/homeController.js"
 import { Router } from "express"
+import movieController from "./controller/movieContorller.js"
 
 const routes = Router();
 routes.use(homeConroller);
+routes.use(movieController);
 
 
 
