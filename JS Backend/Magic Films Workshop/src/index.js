24e927138e4ae855cbf4a8b7d1dae7bcbf1c06c1
Ivaylo Andonov <nikb250nik@gmail.com>
2025-01-19 13:06:54 +0200

    added homeController

--- JS Backend/Magic Films Workshop/src/index.js
+++ JS Backend/Magic Films Workshop/src/index.js
@@ -1,21 +1,21 @@
 import express from "express";
 import  handlebars  from "express-handlebars";
+import homeConroller from "./controller/homeController.js"
 
 const app = express();
 app.engine(`hbs`,handlebars.engine({
     extname:`hbs`
 }));
+
 app.set(`view engine`,`hbs`);
 app.set(`views`,`./src/views`);
 
-app.use(`/static`,express.static(`src/public`))
-app.get(`/`,(req,res)=>{
-    res.render(`home`);
-});
-app.get(`/about`,(req,res)=>{
-    res.render(`about`);
-});
+app.use(`/static`,express.static(`src/public`));
+app.use(homeConroller)
+
+
 app.get(`*`,(req,res)=>{
     res.render(`404`);
 });
+
 app.listen(5000,()=>console.log(`Server is listening on http://localhost:5000......`));
