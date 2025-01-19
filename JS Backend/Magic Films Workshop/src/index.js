54da87d301080665c9027f70f3a0971bbbd38f74
Ivaylo Andonov <nikb250nik@gmail.com>
2025-01-19 12:28:28 +0200

    show home page

--- JS Backend/Magic Films Workshop/src/index.js
+++ JS Backend/Magic Films Workshop/src/index.js
@@ -6,9 +6,10 @@ app.engine(`hbs`,handlebars.engine({
     extname:`hbs`
 }));
 app.set(`view engine`,`hbs`);
-app.set(`views`,`./src`);
+app.set(`views`,`./src/views`);
 
+app.use(`/static`,express.static(`src/public`))
 app.get(`/`,(req,res)=>{
-    res.send(`home`,);
+    res.render(`home`,{layout:false});
 });
 app.listen(5000,()=>console.log(`Server is listening on http://localhost:5000......`));
