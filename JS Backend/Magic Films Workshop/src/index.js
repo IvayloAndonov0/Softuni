55764c950cd708d2ceaa86d6c024573b6e85b822
Ivaylo Andonov <nikb250nik@gmail.com>
2025-01-19 11:41:47 +0200

    add handlebars

--- JS Backend/Magic Films Workshop/src/index.js
+++ JS Backend/Magic Films Workshop/src/index.js
@@ -1,8 +1,14 @@
-import express from "express"
+import express from "express";
+import  handlebars  from "express-handlebars";
 
 const app = express();
+app.engine(`hbs`,handlebars.engine({
+    extname:`hbs`
+}));
+app.set(`view engine`,`hbs`);
+app.set(`views`,`./src`);
 
 app.get(`/`,(req,res)=>{
-    res.send(`it works`);
+    res.send(`home`,);
 });
 app.listen(5000,()=>console.log(`Server is listening on http://localhost:5000......`));
