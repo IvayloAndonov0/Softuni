a18b7a0226df0f66ec0c618300d5f9fd399d10cf
Ivaylo Andonov <nikb250nik@gmail.com>
2025-01-22 18:16:58 +0200

    set up mongoose

--- JS Backend/Magic Films Workshop/src/index.js
+++ JS Backend/Magic Films Workshop/src/index.js
@@ -2,8 +2,21 @@ import express from "express";
 import  handlebars  from "express-handlebars";
 import routes from "./routes.js"
 import showRatingHelper from "./helpers/rating-helper.js";
+import mogoose from "mongoose"
+import mongoose from "mongoose";
 
 const app = express();
+
+
+try {
+    const uri= `mongodb://localhost:27017/magic-movies-jan2025`;
+    await mongoose.connect(uri);
+    console.log(`DB connected successfuly!`);
+    
+} catch (err) {
+    console.error(err.message)
+}
+
 app.engine(`hbs`,handlebars.engine({
     extname:`hbs`,
     helpers:{showRating : showRatingHelper}
@@ -14,6 +27,9 @@ app.set(`views`,`./src/views`);
 
 app.use(`/static`,express.static(`src/public`));
 app.use(express.urlencoded({extended:false}));
+
+
+
 app.use(routes);
 
 
