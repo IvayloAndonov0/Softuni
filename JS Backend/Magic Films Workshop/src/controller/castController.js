8a33e793f267093daa5957f2b8ff4d0642e555a4
Ivaylo Andonov <nikb250nik@gmail.com>
2025-01-25 12:50:01 +0200

    added cast functionality

--- JS Backend/Magic Films Workshop/src/controller/castController.js
+++ JS Backend/Magic Films Workshop/src/controller/castController.js
@@ -1,10 +1,16 @@
 import { Router } from "express";
+import castService from "../services/castService.js";
 
 const castController = Router();
 
 
 castController.get(`/create`,(req,res)=>{
     res.render(`cast/create`);
+});
+castController.post(`/create`, async (req,res)=>{
+    const newCast = req.body;
+    await castService.create(newCast);
+    res.redirect(`/`);
 });
 
 export default castController;
