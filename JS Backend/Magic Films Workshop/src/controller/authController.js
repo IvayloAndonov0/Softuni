d9665300eea063050560db18fbeb594c2fec304f
Ivaylo Andonov <nikb250nik@gmail.com>
2025-01-29 18:28:31 +0200

    add user model

--- JS Backend/Magic Films Workshop/src/controller/authController.js
+++ JS Backend/Magic Films Workshop/src/controller/authController.js
@@ -5,7 +5,13 @@ const authController = Router();
 
 authController.get(`/register`,(req,res)=>{
     res.render(`auth/register`)
-})
+});
+
+authController.post(`/register`, async (req,res)=>{
+    const userData = req.body;
+});
+
+
 
 
 export default authController;
